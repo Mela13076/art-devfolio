6e5433742b91c615a5f564a6e4ed5ebdd9617a4e
Amelia Trevino <trevinoamelia25@gmail.com>
2025-12-10 20:38:15 -0600

    updating types to include extra project info

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,22 @@ export interface Project {
   githubLink?: string;
   demoLink?: string;
   image?: string;
+  slug: string;
+  overview?: string;
+  features?: string[];
+  techStack?: string[];
+  screenshots?: string[];
+  role?: string;
+  duration?: string;
+  challenges?: string[];
+  lessons?: string[];
+  futurePlans?: string[];
+  lastUpdated?: string;
+  videoLink?: string;
+  documentLink?: string[];
 }
+
+
 
 export interface Blog {
   title: string;
